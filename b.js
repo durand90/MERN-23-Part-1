19bc5d468a7e5a053b961e57b1c0933268bea1e1
Fanfan durand <durand90@live.com>
2023-05-03 19:07:18 -0400

    four

--- b.js
+++ b.js
@@ -38,11 +38,10 @@ const beatles = ['Paul', 'George', 'John', 'Ringo'];
 function printNames(names) {
     function actuallyPrintingNames() {
         for (let index = 0; index < names.length; index++) {
-        let name = names[index];
+        const name = names[index];
 
             console.log(name + ' was found at index ' + index);
         }
-        console.log('name and index after loop is ' + name + ':' + index);
     }
     actuallyPrintingNames();
 }
