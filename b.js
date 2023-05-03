e4488527591013fa21281865dd955fb314ede626
Fanfan durand <durand90@live.com>
2023-05-03 19:02:53 -0400

    three

--- b.js
+++ b.js
@@ -1,13 +1,49 @@
+/*
 var beatles = ['Paul', 'George', 'John', 'Ringo'];
 function printNames(names) {
-    function actuallyPrintingNames(){
-    for (var index = 0; index < names.length; index++) {
-        var name = names[index];
-        
-        console.log(name + ' was found at index ' + index);
+    function actuallyPrintingNames() {
+        for (var index = 0; index < names.length; index++) {
+            var name = names[index];
+
+            console.log(name + ' was found at index ' + index);
+        }
+        console.log('name and index after loop is ' + name + ':' + index);
     }
-    console.log('name and index after loop is ' + name + ':' + index);
+    actuallyPrintingNames();
 }
-actuallyPrintingNames();
+printNames(beatles);
+*/
+
+
+//using const and let 
+
+// const beatles = ['Paul', 'George', 'John', 'Ringo'];
+// function printNames(names) {
+//     function actuallyPrintingNames() {
+//         let index = 0;
+//         let name; 
+//         for (; index < names.length; index++) {
+//          name = names[index];
+
+//             console.log(name + ' was found at index ' + index);
+//         }
+//         console.log('name and index after loop is ' + name + ':' + index);
+//     }
+//     actuallyPrintingNames();
+// }
+// printNames(beatles);
+
+
+const beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+    function actuallyPrintingNames() {
+        for (let index = 0; index < names.length; index++) {
+        let name = names[index];
+
+            console.log(name + ' was found at index ' + index);
+        }
+        console.log('name and index after loop is ' + name + ':' + index);
+    }
+    actuallyPrintingNames();
 }
 printNames(beatles);
